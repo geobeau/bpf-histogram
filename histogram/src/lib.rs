f40f7b6e3f90b742ce996201c43f10c293b86f80
Geoffrey Beausire <geobeausire@gmail.com>
2024-04-19 09:48:48 +0200

    Add a way to provide custom labels

--- histogram/src/lib.rs
+++ histogram/src/lib.rs
@@ -3,41 +3,41 @@ use aya::{
     Pod,
 };
 use std::{hash::Hash, sync::Arc};
-use std::{
-    collections::{BTreeMap, HashMap},
-    marker::PhantomData,
+
+use prometheus::{
+    core::{AtomicF64, Collector, Desc, GenericGaugeVec},
+    proto, GaugeVec, Opts,
 };
 
-use prometheus::{core::{Atomic, AtomicF64, Collector, Desc, GenericGauge, GenericGaugeVec}, proto, Encoder, Gauge, GaugeVec, Opts, Registry, TextEncoder};
-
-
-pub trait Key: Sized + Pod + Eq + PartialEq + Hash + Send + Sync {}
+pub trait Key: Sized + Pod + Eq + PartialEq + Hash + Send + Sync {
+    /// Return keys for the Prometheus label pairs:
+    /// - Can be empty
+    /// - Should be the same order and size as `get_label_values`
+    fn get_label_keys() -> Vec<String>;
+    /// Return values for the Prometheus label pairs:
+    /// - Can be empty
+    /// - Should be the same order as get_label_keys
+    fn get_label_values(&self) -> Vec<String>;
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 #[repr(C)]
-pub struct KeyWrapper<T: Sized + Pod + Eq + PartialEq + Hash + Send + Sync> {
+pub struct KeyWrapper<T: Key> {
     pub bucket: u32,
     pub sub_key: T,
 }
 
-unsafe impl<T: Sized + Pod + Eq + PartialEq + Hash + Send + Sync> Pod for KeyWrapper<T> {}
+unsafe impl<T: Key> Pod for KeyWrapper<T> {}
 
 #[derive(Clone)]
-pub struct Histogram<T: Sized + Pod + Eq + PartialEq + Hash + Send + Sync> {
+pub struct Histogram<T: Key> {
     map: Arc<PerCpuHashMap<MapData, KeyWrapper<T>, u64>>,
     // phantom: PhantomData<T>,
     buckets_metric: GenericGaugeVec<AtomicF64>,
 }
 
-pub struct PerKeyHistogram {
-    buckets: BTreeMap<u64, u64>,
-}
 
-impl PerKeyHistogram {
-    pub fn new_from_map() {}
-}
-
-impl<T: Sized + Pod + Eq + PartialEq + Hash + Send + Sync> Collector for Histogram<T> {
+impl<T: Key> Collector for Histogram<T> {
     fn desc(&self) -> Vec<&Desc> {
         self.buckets_metric.desc()
     }
@@ -50,18 +50,28 @@ impl<T: Sized + Pod + Eq + PartialEq + Hash + Send + Sync> Collector for Histogr
                 Err(_) => None,
             })
             .for_each(|(key, values)| {
-                println!("{:?}", values);
+                let label_keys = key.sub_key.get_label_values();
+                let bucket = key.bucket.to_string();
+                let mut str_label_keys: Vec<&str> = label_keys.iter().map(|x| x.as_str()).collect();
+                str_label_keys.push(bucket.as_str());
+
                 let total = values.iter().sum::<u64>();
-                self.buckets_metric.with_label_values(&[key.bucket.to_string().as_str()]).set(total as f64)
+                self.buckets_metric
+                    .with_label_values(&str_label_keys)
+                    .set(total as f64)
             });
         self.buckets_metric.collect()
     }
 }
 
-impl<T: Sized + Pod + Eq + PartialEq + Hash + Send + Sync> Histogram<T> {
+impl<T: Key> Histogram<T> {
     pub fn new_from_map(map: PerCpuHashMap<MapData, KeyWrapper<T>, u64>) -> Histogram<T> {
         let bucket_opts = Opts::new("test_latency", "test counter help");
-        let buckets_metric = GaugeVec::new(bucket_opts, &["le"]).unwrap();
+        let label_keys = T::get_label_keys();
+        let mut str_label_keys: Vec<&str> = label_keys.iter().map(|x| x.as_str()).collect();
+        str_label_keys.push("le");  // le contains the lower/equal buckets for histogram
+
+        let buckets_metric = GaugeVec::new(bucket_opts, &str_label_keys).unwrap();
         Histogram {
             map: Arc::from(map),
             buckets_metric,
